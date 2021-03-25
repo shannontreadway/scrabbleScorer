e833c197677548984dec2ddcef09bcffe86423cb
shannontreadway <shannon.treadway@gmail.com>
2021-03-25 15:07:21 -0500

    working input and scoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Player1Component } from './player1/player1.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { Player1Component } from './player1/player1.component';
     Player1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/player1/player1.component.ts
+++ src/app/player1/player1.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Player1Component implements OnInit {
   words: string[] = [];
+  wordToScore: string = "";
   wordScores: number[] = [];
   wordsWithPoints: WordPtObj[] = [];
   wordForObject: string = "";
   gameScore: number = 0;
   score: number = 0;
+  allTiles: boolean = false;
   scrabblePoints = {a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10};
 
   constructor() { }
@@ -25,21 +27,30 @@ export class Player1Component implements OnInit {
     return this.words.unshift(newWord);
   }
 
-  scoreWord(newWord: string){
+  bonusPoints() {
+    this.allTiles = true;
+    return;
+  }
+
+  scoreWord(wordToScore: string){
     this.score = 0;
-    newWord = newWord.toLowerCase();
-    let newWordArray = newWord.split("");
+    wordToScore = wordToScore.toLowerCase();
+    let newWordArray = wordToScore.split("");
     for (let i = 0; i < newWordArray.length; i++){
       this.score += this.scrabblePoints[newWordArray[i]];
     };
+    if(this.allTiles){
+      this.score += 75;
+    }
     return this.wordScores.unshift(this.score);
   }
 
-  updateWordsAndScores(newWord: string) {
+  updateWordsAndScores(newWord: string, wordToScore: string) {
     this.addWord(newWord);
-    this.scoreWord(newWord);
-    let wordObject = new WordPtObj(this.wordForObject, this.score)
+    this.scoreWord(wordToScore);
+    let wordObject = new WordPtObj(this.wordForObject, this.score, this.allTiles);
     this.wordsWithPoints.unshift(wordObject);
+    this.allTiles=false;
     this.gameScore += this.score;
     return;
   }
@@ -49,9 +60,11 @@ export class Player1Component implements OnInit {
 class WordPtObj{
   word: string;
   pointValue: number;
+  allTilesUsed: boolean;
 
-  constructor(word: string, pointValue: number) {
+  constructor(word: string, pointValue: number, allTilesUsed: boolean) {
     this.word = word;
     this.pointValue = pointValue;
+    this.allTilesUsed = allTilesUsed;
   }
 }
