9bed99f56ce61ebfb6a1a660f22c52bf472d5893
shannontreadway <shannon.treadway@gmail.com>
2021-06-20 13:25:39 -0500

    fixed something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Scrabble Scorer';
-  hidden: boolean = false;
-
-  toggleDisplay() {
-    this.hidden = true;
-    return;
-  }
 }
