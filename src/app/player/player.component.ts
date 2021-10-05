ebbfbc25b794394ba9678b90dd3472cad3b384c4
shannontreadway <shannon.treadway@gmail.com>
2021-10-05 12:56:56 -0500

    changed background colors

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,12 +1,40 @@
-import { isLoweredSymbol } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { isLoweredSymbol } from "@angular/compiler";
+import { Component, OnInit } from "@angular/core";
 
-const scrabblePoints = {a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10};
+const scrabblePoints = {
+  a: 1,
+  e: 1,
+  i: 1,
+  o: 1,
+  u: 1,
+  l: 1,
+  n: 1,
+  r: 1,
+  s: 1,
+  t: 1,
+  d: 2,
+  g: 2,
+  b: 3,
+  c: 3,
+  m: 3,
+  p: 3,
+  f: 4,
+  h: 4,
+  v: 4,
+  w: 4,
+  y: 4,
+  k: 5,
+  j: 8,
+  x: 8,
+  q: 10,
+  z: 10,
+  " ": 0,
+};
 
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  selector: "app-player",
+  templateUrl: "./player.component.html",
+  styleUrls: ["./player.component.css"],
 })
 export class PlayerComponent implements OnInit {
   wordsWithPoints: WordPtObj[] = [];
@@ -15,21 +43,20 @@ export class PlayerComponent implements OnInit {
   newWord: string = "";
   wordToScore: string = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  scoreWord(wordToScore: string, allTilesUsed: boolean): number{
+  scoreWord(wordToScore: string, allTilesUsed: boolean): number {
     let score = 0;
     for (const char of wordToScore.toLowerCase()) {
       score += scrabblePoints[char];
-    };
-    if(allTilesUsed){
+    }
+    if (allTilesUsed) {
       score += 75;
-    };
+    }
     return score;
-  };
+  }
 
   updateWordsAndScores() {
     const score: number = this.scoreWord(this.wordToScore, this.allTilesUsed);
@@ -40,11 +67,10 @@ export class PlayerComponent implements OnInit {
     this.newWord = "";
     this.wordToScore = "";
     return;
-  };
-  
-};
+  }
+}
 
-class WordPtObj{
+class WordPtObj {
   word: string;
   pointValue: number;
   allTilesUsed: boolean;
@@ -53,5 +79,5 @@ class WordPtObj{
     this.word = word;
     this.pointValue = pointValue;
     this.allTilesUsed = allTilesUsed;
-  };
-};
+  }
+}
